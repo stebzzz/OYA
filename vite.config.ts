ba397fa0d4b30c0a3331824151673f1bb9c73808
stebzzz <145580377+stebzzz@users.noreply.github.com>
2025-06-11 11:54:46 +0200

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
